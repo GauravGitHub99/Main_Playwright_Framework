16d6d9587af4eeab285cd20cf2f1381cb9ea86a0
Gaurav Suryawanshi <suryawanshig1999@gmail.com>
2024-10-24 09:18:37 +0530

    push

---
+++ tests/JS programs/helpers/printHelper.js
@@ -0,0 +1,17 @@
+export function printAge(Age) {
+    console.log(Age)
+}
+
+class CustomerDetails {
+
+    printFirstName(FirstName){
+        console.log(FirstName)
+    }
+
+    printLastName(LastName){
+        console.log(LastName)
+    }
+
+}
+
+export const CustomerDetails = new CustomerDetails()
